b837f37d60754e8c098837b733165ddd11e96800
GaneshPatil1806 <ganeshpatil180603@gmail.com>
2023-12-26 18:27:43 +0530

    geolocation and Profanity-module

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,17 +5,39 @@ const socket = io()
 // })
 
 socket.on('message',(message)=>{
-    console.log('The message ',message)
+    console.log(message)
 })
 
 document.querySelector('#message-form').addEventListener('submit',(e)=>{
     e.preventDefault()
 
-    const message = document.querySelector('input').value
-    socket.emit('sentall',message)
+    //const message = document.querySelector('input').value
+
+    const message = e.target.elements.message_in.value
+    socket.emit('sentall',message,(error)=>{
+        if(error){
+            return console.log(error);
+        }
+        
+        console.log('The message was delivered!');
+    })
 })
 
 // document.querySelector('#increment').addEventListener('click',()=>{
 //     console.log('Clicked');
 //     socket.emit('increment')
 // })
+
+document.querySelector('#send-location').addEventListener('click',()=>{
+    if(!navigator.geolocation){
+        return alert('Geolocation is not supported by your browser!')
+    }
+
+    navigator.geolocation.getCurrentPosition((position)=>{
+        // const msg = 'Location: '+  position.coords.latitude + ', ' + position.coords.longitude;
+        socket.emit('sendLocation',{
+            latitude:position.coords.latitude,
+            longitude:position.coords.longitude
+        })
+    })
+})
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require("path")
 const http = require("http")
 const express = require("express")
 const socketio = require("socket.io")
+const Filter = require('bad-words')
 
 const app = express();
 // not necessury 
@@ -20,12 +21,24 @@ io.on('connection', (socket) => {
     console.log("New websocket connection")
     socket.emit('welcome',"Welcome!")
 
+    socket.broadcast.emit('message','A new user has joined')
     // socket.on('sentall',(message)=>{
     //     console.log(message);
     // })
 
-    socket.on('sentall',(message)=>{
+    socket.on('sentall',(message,callback)=>{
+        const filter = new Filter()
+
+        if(filter.isProfane(message)){
+            return callback('Profanity is not allowed!')
+        }
+        
         io.emit('message',message)
+        callback()
+    })
+
+    socket.on('sendLocation',(message)=>{
+        io.emit('message',`https://google.com/maps?q=${ message.latitude},${message.longitude}`)
     })
     
     //socket.emit('countUpdated',count)
@@ -37,6 +50,10 @@ io.on('connection', (socket) => {
         // socket.emit('countUpdated',count)
     //     io.emit('countUpdated',count)
     // })
+
+    socket.on('disconnect',()=>{
+        io.emit('message','A user has left');
+    })
 });
 
 server.listen(port,()=>{
